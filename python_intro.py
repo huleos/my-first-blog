9d8b41b37bede9e7cb7b95cdbe85f44f32865350
Julio Cesar Montenegro <creativo@686studio.com>
2016-05-25 16:10:46 -0700

    my first commit django tut

---
+++ python_intro.py
@@ -0,0 +1,21 @@
+# def hi(name):
+# 		pass
+# 		if name == 'Ola':
+# 				print('Hey Ola!')
+# 		elif name == 'huleos':
+# 				print('Hey huleos')
+# 		else:
+# 				print('Hey anonymous!')
+
+# hi('huleos')
+
+def hi(name):
+		print('Hola ' + name + '!')
+
+girls = ['Renata', 'Ana Karen', 'Emma', 'Victoria']
+for name in girls:
+	hi(name)
+	print('Next Girl =>')
+
+for i in range(1,7):
+		print(i)
